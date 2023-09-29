7511ad2258902f3c85949ef41b350647fbbc25b5
gulnabi-afridi <109666507+gulnabi-afridi@users.noreply.github.com>
2023-09-29 16:18:05 +0500

    updated herosection

--- components/Home/HeroSection.tsx
+++ components/Home/HeroSection.tsx
@@ -1,21 +1,33 @@
 import React from "react";
+import { Link as ScrollLink } from "react-scroll";
 
 const HeroSection = () => {
   return (
-    <div className="w-full">
-      <div className="w-full max-w-[1280px] m-auto sm:gap-4 gap-1 md:px-4 px-3 h-[calc(100vh-90px)] flex flex-col justify-center items-center">
+    <div className="w-full flex">
+      <div className="w-full max-w-[1280px] m-auto sm:gap-4 gap-1 md:px-4 px-3 h-[calc(100vh-90px)] flex flex-col justify-center sm:mt-0 -mt-28 items-center">
         {/* title -------->  */}
         <p className="text-[40px] leading-[43px] sm:text-[50px] md:text-[68px] text-center sm:leading-14 md:leading-16 uppercase font-black text-yellow-main font-incon tracking-wider">
           PRIVATE AUCTIONS
         </p>
         {/* Powered by ------> */}
-        <p className="text-white/80 text-[16px] sm:text-[26px] md:text-[30px] font-incon font-medium">
+        <p className="text-white/80 text-[18px] sm:text-[26px] md:text-[30px] font-incon font-medium">
           Powered by company
         </p>
         {/* try it now button ------> */}
-        <button className="mt-5 text-[24px] text-black font-incon font-semibold tracking-normal hover:opacity-80 bg-yellow-main px-8 py-2 rounded-md">
+        <ScrollLink
+          to="formButtonSection"
+          spy={true}
+          smooth={true}
+          hashSpy={true}
+          offset={0}
+          duration={800}
+          isDynamic={true}
+          ignoreCancelEvents={false}
+          spyThrottle={500}
+          className="mt-5 cursor-pointer text-[24px] text-black font-incon font-semibold tracking-normal hover:opacity-80 bg-yellow-main px-8 py-[6px] sm:py-2 rounded-md"
+        >
           Try it Now
-        </button>
+        </ScrollLink>
       </div>
     </div>
   );
