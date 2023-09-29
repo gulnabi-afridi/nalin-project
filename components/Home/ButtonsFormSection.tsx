99f6c6d83f6dfb26bc23bf0d3bc35e2459d8347c
gulnabi-afridi <109666507+gulnabi-afridi@users.noreply.github.com>
2023-09-29 16:19:58 +0500

    server side issues solved

--- components/Home/ButtonsFormSection.tsx
+++ components/Home/ButtonsFormSection.tsx
@@ -105,7 +105,13 @@ const ButtonsFormSection = () => {
               >
                 {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14].map(
                   (item, index) => {
-                    return <Button text={`button`} style="w-full"></Button>;
+                    return (
+                      <Button
+                        key={index}
+                        text={`button`}
+                        style="w-full"
+                      ></Button>
+                    );
                   }
                 )}
               </div>
