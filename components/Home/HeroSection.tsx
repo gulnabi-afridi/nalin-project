5e0dbd52eeaeac01fe7e043a6542d5b4d7246f90
gulnabi-afridi <109666507+gulnabi-afridi@users.noreply.github.com>
2023-09-29 14:20:28 +0500

    herosection is responsive now

--- components/Home/HeroSection.tsx
+++ components/Home/HeroSection.tsx
@@ -3,13 +3,13 @@ import React from "react";
 const HeroSection = () => {
   return (
     <div className="w-full">
-      <div className="w-full max-w-[1280px] m-auto md:px-4 px-3 h-[calc(100vh-90px)] flex flex-col justify-center items-center -mt-8">
+      <div className="w-full max-w-[1280px] m-auto sm:gap-4 gap-1 md:px-4 px-3 h-[calc(100vh-90px)] flex flex-col justify-center items-center -mt-10">
         {/* title -------->  */}
-        <p className="text-[68px] uppercase font-black text-yellow-main font-incon tracking-wider">
+        <p className="text-[40px] leading-[43px] sm:text-[50px] md:text-[68px] text-center sm:leading-14 md:leading-16 uppercase font-black text-yellow-main font-incon tracking-wider">
           PRIVATE AUCTIONS
         </p>
         {/* Powered by ------> */}
-        <p className="text-white/80 text-[30px] font-incon font-medium">
+        <p className="text-white/80 text-[16px] sm:text-[26px] md:text-[30px] font-incon font-medium">
           Powered by company
         </p>
       </div>
